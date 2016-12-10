7bb26fc00e3fdca63357701851b9edb4364999b7
takashift <e145771@ie.u-ryukyu.ac.jp>
2016-12-11 01:58:54 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,19 @@
 public class Main {
     public static void main(String[] args) {
         String str = null;
-        str.length();
+        try {
+            str.length();
+        }
+        catch( NullPointerException e) {
+            System.out.println(" 　　 ∧＿∧\n" +
+                    "str→（　´∀｀）＜ ぬるぽ\n");
+            System.out.println("　 （　・∀・）　　　|　|　ｶﾞｯ\n" +
+                    "　と　　　　）　 　 |　|\n" +
+                    "　　 Ｙ　/ノ　　　 人\n" +
+                    "　　　 /　）　 　 < 　>__Λ∩\n" +
+                    "　 ＿/し'　／／. Ｖ｀Д´）/ ←str\n" +
+                    "　（＿フ彡　　　　　 　　/");
+            e.printStackTrace();
+        }
     }
 }
